616e93166e1cbb93ce9fc9deec392f93bdf79123
Barbosa885 <gustavosports7@gmail.com>
2023-05-18 18:16:34 -0300

    feat: Captures the input state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import { GoSearch } from "react-icons/go";
-import api from "./services/api";
+import api from "./services/api"
 
 export default function App() {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
 
   useEffect(() => {
-    api.get();
-  }, []);
+    api
+    .get()
+  }, [])
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-b from-orange-500 to-yellow-300">
@@ -17,14 +22,16 @@ export default function App() {
         </h1>
         <div className="flex items-center">
           <input
-            className="placeholder:italic bg-white border border-slate-300 w-full p-2 pl-8"
+            className="placeholder:italic bg-white border border-slate-300 w-50 p-2 pl-8"
             placeholder="Insira aqui o nome da cidade..."
+            value={value}
+            onChange={handleChange}
           />
           <GoSearch className="text-slate-700 absolute ml-3" />
         </div>
         <hr />
       </div>
-      <div className="flex flex-col pt-4">
+      <div className="pt-4">
         <h3 className="text-white text-3xl font-medium">Capitais</h3>
         <div className="flex space-x-2">
           <span className="font-light">Min</span>
