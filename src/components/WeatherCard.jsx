4d1ad7c6fb7efc0f1d274175478b892f0361bebb
Barbosa885 <gustavosports7@gmail.com>
2023-11-20 23:11:37 -0300

    feat: implements intersectionObserver method to emojis

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { BsArrowDown, BsArrowUp } from "react-icons/bs";
 
 export function WeatherCard(props) {
-
   const determineEmoji = () => {
     switch (true) {
       case props.temp > 25:
@@ -13,16 +12,44 @@ export function WeatherCard(props) {
         return "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Smiling%20Face%20with%20Sunglasses.png"
     }
   }
+  const [emoji, setEmoji] = useState(determineEmoji());
+  const emojiRef = useRef();
+
+
+  useEffect(() => {
+    setEmoji(determineEmoji());
+  }, [props.temp]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(!entry.isIntersecting) return;
+        entry.target.src = entry.target.getAttribute("data-src");
+        observer.unobserve(entry.target);
+      });
+    })
+
+    if (emojiRef.current) {
+      observer.observe(emojiRef.current);
+    }
+
+    return () => {
+      if (emojiRef.current) {
+        observer.unobserve(emojiRef.current);
+      }
+    };
+  }, [emoji]) 
+
 
   return (
     <div className="bg-orange-50 p-4 rounded-lg shadow-md space-y-4 flex flex-col items-center">
       <p className="flex space-x-2">
         <span>{props.city}</span>
-        <img src={`https://countryflagsapi.netlify.app/flag/${props.flag}.svg`} className="max-w-[20px]"/>
+        <img alt={`bandeira ${props.flag}`} src={`https://countryflagsapi.netlify.app/flag/${props.flag}.svg`} className="max-w-[20px]"/>
       </p>
       <h1 className="flex text-6xl font-bold items-center">
         {props.temp}°C 
-        <img src={determineEmoji()} alt="Emoji" className="w-16 h-16 ml-4"/>
+        <img ref={emojiRef} data-src={determineEmoji()} alt="Emoji" className="w-16 h-16 ml-4"/>
       </h1>
       <div className="inline-grid grid-cols-2 gap-4">
         <div className="flex items-center justify-center">
